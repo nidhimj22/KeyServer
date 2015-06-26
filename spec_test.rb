00d005bc8de62e036cfd8ce299f410b740335009
Nidhi Makhijani <nidhimj22@gmail.com>
2015-06-26 15:42:53 +0530

    rspec tests added

---
+++ spec_test.rb
@@ -0,0 +1,60 @@
+$LOAD_PATH << File.dirname(__FILE__)
+
+require 'rubygems'
+require 'sinatra'
+require 'redis'
+require 'main'
+require 'keyserver'
+
+redis = Redis.new
+redis.flushall
+key_server=KeyServer.new
+
+describe "MainTest" do
+
+  before(:all) do  
+    puts "==================RSPEC TESTS TO CHECK ENDPOINTS==========================="
+
+  end
+
+  it "checks generate_key endpoint" do
+    expect(key_server.generate(redis)).to match(/Key Generated/)
+  end
+
+  it "checks get_key endpoint" do
+    key = key_server.get(redis)
+    expect(key).to match(/[a-f0-9]*/)
+  end
+
+  it "checks unblock_key endpoint" do
+    key_server.generate(redis)
+    key = key_server.get(redis)
+    expect(key_server.unblock(redis,key)).to match(/Key unblocked/)
+  end
+  
+  it "checks unblock_key endpoint" do
+    expect(key_server.unblock(redis,"ac0dffd5acbbeb637dd987500a8b9528")).to match(/Key not exists/)
+  end
+  
+  it "checks delete_key endpoint" do
+    expect(key_server.delete(redis,"ac0dffd5acbbeb637dd987500a8b9528")).to match(/Key not exists/)
+  end
+  
+  it "checks delete_key endpoint" do
+    key_server.generate(redis)
+    key = key_server.get(redis)
+    expect(key_server.delete(redis,key)).to match(/Key deleted/)
+  end
+  
+  it "checks keep_alive endpoint" do
+    key_server.generate(redis)
+    key = key_server.get(redis)
+    expect(key_server.keep_alive(redis,key)).to match(/Key life extended/)
+  end
+  
+  it "checks keep_alive endpoint" do
+    expect(key_server.keep_alive(redis,"ac0dffd5acbbeb637dd987500a8b9528")).to match(/Key not exists/)
+  end
+
+end
+
