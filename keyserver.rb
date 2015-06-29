386b7df73a7e2d962c2a6ec0c4749f4882a8231a
Nidhi Makhijani <nidhimj22@gmail.com>
2015-06-29 14:54:48 +0530

    correction

--- keyserver.rb
+++ keyserver.rb
@@ -1,69 +1,62 @@
 require 'securerandom'
 #require 'redis'
 
+#set - UNBLOCKED
+#list - BLOCKED
+
 class KeyServer
-  
-  def initialize  
+
+  def initialize
   end
 
 
-  def generate(redis)
+  def generate(redis) #O(1)
     random_key = SecureRandom.hex
-    redis.sadd('UNBLOCKED',random_key)
-    redis.setex(random_key,300,Time.now)
-
     if random_key.nil?
       "Key Generation failed"
-    else
-      "Key Generated"
     end
-
+    redis.setex(random_key,300,Time.now) 
+    redis.sadd('UNBLOCKED',random_key) #unblocked set (unsorted set)
+    "Key Generated"
   end
-  
 
-  def get(redis)
+
+  def get(redis) #O()
     key = redis.spop('UNBLOCKED')
     if key.nil?
-      key = get_expired_key(redis)
-    else 
+      key = key_from_blocked(redis) #amortized O(1) complexity
+    else
       redis.set(key,Time.now)
       redis.lpush('BLOCKED',key) 
     end
     key
   end
-   
+
   def unblock(redis,key)
-    if redis.exists(key)     
-      if redis.sadd('UNBLOCKED',key)
-         redis.lrem('BLOCKED',1,key) 
-         "Key unblocked"
-      else
-        "Key unblocked"
-      end
+    if redis.exists(key)
+      redis.lrem('BLOCKED',1,key)
+      redis.sadd('UNBLOCKED',key)
+      "Key unblocked"
     else
       "Key not exists"
     end
   end
 
   def delete(redis,key)
-
     if redis.exists(key)
       if redis.del(key)==1
-        if redis.sismember('UNBLOCKED',key)
-	  redis.srem('UNBLOCKED',key)
-	end               
-	redis.lrem('BLOCKED',1,key) 
-        "Key deleted"
+          redis.srem('UNBLOCKED',key) 
+          redis.lrem('BLOCKED',1,key)
+          "Key deleted"
       else
         "Key unable to delete"
       end
-    else 
+    else
       "Key not exists"
     end
-
   end
 
-      
+
   def keep_alive(redis,key)
     if redis.exists(key)
        redis.setex(key,300,Time.now)
@@ -73,21 +66,24 @@ class KeyServer
     end
   end
 
-    
-  def get_expired_key(redis)
-    key = redis.rpop('BLOCKED')
-    if not key.nil?
-      time_stamp = redis.get(key)
-      if (Time.parse(time_stamp)-Time.now).abs >=60
-        redis.lpush('BLOCKED',key)
-        redis.set(key,Time.now)
+
+  def key_from_blocked(redis)
+    temp_key = redis.rpop('BLOCKED')
+
+    if temp_key.nil?
+      nil
+    else
+      temp_key_time = redis.get(temp_key)
+      if (Time.parse(temp_key_time)-Time.now).abs >=60
+        redis.lpush('BLOCKED',temp_key)
+        redis.set(temp_key,Time.now)
         key
       else
-        redis.rpush('BLOCKED',key)
+        redis.rpush('BLOCKED',temp_key)
         nil
       end
-    end 
-    nil     
+    end
+
   end
 end
 
